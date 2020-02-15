01a3dec71fb6d2c196516934615639c3934a3675
Wonz5130 <wangzhou5130@gmail.com>
2020-02-15 16:35:18 +0800

    :green_heart:Update 0451 Code
    :green_heart:Update 0451 Code

--- solutions/0451-Sort-Characters-By-Frequency/0451.py
+++ solutions/0451-Sort-Characters-By-Frequency/0451.py
@@ -4,12 +4,35 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        import collections  # 需要导入collections
-        count = collections.Counter(s).most_common()  # Counter类就能统计每个字符出现的次数, 使用most_common函数就能按次序排列
-        res = ''
-        for char, cnt in count:
-            res += char*cnt  # 字符与次数相乘
-        return res
+        # # 法一：collections.Counter
+        # import collections  # 需要导入collections
+        # count = collections.Counter(s).most_common()  # Counter类就能统计每个字符出现的次数, 使用most_common函数就能按次序排列
+        # res = ''
+        # for char, cnt in count:
+        #     res += char*cnt  # 字符与次数相乘
+        # return res
+        
+        # 法二：桶排序
+        import collections
+
+        # 统计字符个数
+        ret = []
+        count_frequency = collections.defaultdict(int)
+        for i in s:
+            count_frequency[i] += 1  # 统计字符个数
+        
+        # 桶排序
+        buckets = [[] for i in range(len(s) + 1)]
+        for i in count_frequency:  # i是字符
+            # print (i)
+            buckets[count_frequency[i]].extend(i * count_frequency[i])
+        
+        # 打印排完序的字符串
+        for i in buckets[::-1]:  # -1表示逆序
+            if(i):
+                ret.extend(i)
+        return ''.join(ret)
+
         # return ''.join(c*-n for n,c in sorted((-s.count(c),c)for c in set(s)))  # 一行代码也能解决
    
 
