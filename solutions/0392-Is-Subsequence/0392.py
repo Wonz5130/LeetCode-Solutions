23574521a438645af14dbc32d8edf5a48862e470
Wonz5130 <wangzhou5130@gmail.com>
2020-02-20 12:30:19 +0800

    :green_heart:Upload 0392 Code
    :green_heart:Upload 0392 Code

---
+++ solutions/0392-Is-Subsequence/0392.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
+
+if __name__ == "__main__":
+    s = "abc"
+    t = "ahbgdc"
+    print(Solution().isSubsequence(s, t))
