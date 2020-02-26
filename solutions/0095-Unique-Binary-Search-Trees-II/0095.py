c2519a8a40a9ad5d240c134b84d25a66a551e69e
Wonz5130 <wangzhou5130@gmail.com>
2020-02-26 20:18:22 +0800

    :green_heart:Update 0095 Code
    :green_heart:Update 0095 Code

--- solutions/0095-Unique-Binary-Search-Trees-II/0095.py
+++ solutions/0095-Unique-Binary-Search-Trees-II/0095.py
@@ -1,11 +1,11 @@
-from typing import List
+# from typing import List
 
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
     def generateTrees(self, n: int) -> List[TreeNode]:
@@ -29,7 +29,3 @@ class Solution:
                     root.right = right_node
                     ans.append(root)
         return ans
-
-if __name__ == "__main__":
-    n = 3
-    print(Solution().generateTrees(n))
