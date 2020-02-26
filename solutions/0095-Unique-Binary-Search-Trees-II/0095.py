cb69cb3e894e5d3af522ec1f6f2836cf124969f7
Wonz5130 <wangzhou5130@gmail.com>
2020-02-26 15:16:54 +0800

    :green_heart:Upload 0095 Code
    :green_heart:Upload 0095 Code

---
+++ solutions/0095-Unique-Binary-Search-Trees-II/0095.py
@@ -0,0 +1,35 @@
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n == 0:
+            return []
+        else:
+            return self.generateTreesDFS(1, n)
+    
+    # DFS
+    def generateTreesDFS(self, left, right):
+        if left > right:
+            return [None]
+        ans = []
+        for i in range(left, right + 1):
+            left_nodes = self.generateTreesDFS(left, i -  1)  # all possible left subtrees if i is choosen to be a root
+            right_nodes = self.generateTreesDFS(i + 1, right)  # all possible right subtrees if i is choosen to be a root
+            for left_node in left_nodes:
+                for right_node in right_nodes:
+                    root = TreeNode(i)
+                    root.left = left_node
+                    root.right = right_node
+                    ans.append(root)
+        return ans
+
+if __name__ == "__main__":
+    n = 3
+    print(Solution().generateTrees(n))
