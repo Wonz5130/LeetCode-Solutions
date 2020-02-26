a6bbbb5b250c4dc69d12b175b1be79b54ab8e220
Wonz5130 <wangzhou5130@gmail.com>
2020-02-26 22:47:10 +0800

    :green_heart:Update 0279 Code
    add solution two

--- solutions/0279-Perfect-Squares/0279.py
+++ solutions/0279-Perfect-Squares/0279.py
@@ -1,5 +1,6 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+        # solution one: BFS
         q = [(n, 0)]
         visited = [False for i in range(n + 1)]  # initialize all False
         visited[n] = True
@@ -18,6 +19,22 @@ class Solution:
                 
                 i += 1
                 Num = num - i ** 2
+        
+        # solution two: Lagrange's Four-square Theorem
+        while n % 4 == 0:  # reduce n
+            n /= 4
+
+        if n % 8 == 7:
+            return 4
+
+        a = 0
+        while a ** 2 <= n:
+            b = int((n - a ** 2) ** 0.5)
+            if a ** 2 + b ** 2 == n:
+                return (not not a) + (not not b)  # whether a and b are positive integers
+            a += 1
+
+        return 3
 
 if __name__ == "__main__":
     n = 13
