6b8a91f30c17bf25bd75370b9bbecae33d47ceec
Wonz5130 <wangzhou5130@gmail.com>
2020-03-05 20:30:05 +0800

    :green_heart:Update 0047 Code
    :green_heart:Update 0047 Code

--- solutions/0047-Permutations-II/0047.py
+++ solutions/0047-Permutations-II/0047.py
@@ -15,7 +15,7 @@ class Solution:
                 self.dfs(nums[:i] + nums[i + 1:], res, path + [nums[i]])
         
         # solution two: backtracking
-        nums.sort()
+        nums.sort()  # sort at first
         self.res = []
         check = [0 for _ in range(len(nums))]
 
@@ -23,20 +23,22 @@ class Solution:
         return self.res
     
     def dfs(self, sol, nums, check):
+        # 1.valid result
         if len(sol) == len(nums):
             self.res.append(sol)
             return
         
         for i in range(len(nums)):
-            # pruning
+            # 3.pruning
             if check[i] == 1:  # used
                 continue
             if i > 0 and nums[i] == nums[i - 1] and not check[i - 1]:
                 continue
 
+            # 2.backtrack and update check
             check[i] = 1
             self.dfs(sol + [nums[i]], nums, check)
-            check[i] = 0  # after backtracking, should change check[i]
+            check[i] = 0  # after backtracking, should update check[i]
 
 if __name__ == "__main__":
     nums = [1,1,2]
