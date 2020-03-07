2446c5fe81846a36bbc70535c04000f8377d34fc
Wonz5130 <wangzhou5130@gmail.com>
2020-03-07 13:18:24 +0800

    :green_heart:Update 0039 Code
    :green_heart:Update 0039 Code

--- solutions/0039-Combination-Sum/0039.py
+++ solutions/0039-Combination-Sum/0039.py
@@ -6,7 +6,7 @@ class Solution:
         if n == 0:
             return []
         
-        # accelerate
+        # accelerate 剪枝提速，非必需
         candidates.sort()
 
         path, res = [], []
@@ -14,17 +14,17 @@ class Solution:
         return res
     
     def dfs(self, candidates, start, n, path, res, target):
-        # 1.valid result
+        # 1.valid result 递归终止情况
         if target == 0:
             res.append(path[:])
             return
         
         for i in range(start, n):
             tmp = target - candidates[i]
-            # 3.pruning
+            # 3.pruning 剪枝
             if tmp < 0:
                 break
-            # 2.backtrack and update
+            # 2.backtrack and update 回溯以及更新 path
             path.append(candidates[i])
             self.dfs(candidates, i, n, path, res, tmp)
             path.pop()
