210ad13c996967243d9b88eae7eb89ee63e9a9ca
Wonz5130 <wangzhou5130@gmail.com>
2020-03-08 15:47:07 +0800

    :green_heart:Update 1375 Code
    :green_heart:Update 1375 Code

--- solutions/1375-Bulb-Switcher-III/1375.py
+++ solutions/1375-Bulb-Switcher-III/1375.py
@@ -10,7 +10,7 @@ class Solution:
             flag[light[i]] = True  # turn on bulb开灯
             cnt_light += 1
             max_i = max(max_i, light[i])
-            # 已开灯数量 = 当前记录的右边界：灯变蓝计数 + 1
+            # 已开灯数量 = 当前记录的最大编号：灯变蓝计数 + 1
             if cnt_light == max_i:
                 cnt_blue += 1
         return cnt_blue
