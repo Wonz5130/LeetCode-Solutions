2a0173d24ab31940889ae443de1a25a3d7e226ea
Wonz5130 <wangzhou5130@gmail.com>
2020-03-09 19:07:11 +0800

    :green_heart:Update 0121 Code
    :green_heart:Update 0121 Code

--- solutions/0121-Best-Time-to-Buy-and-Sell-Stock/0121.py
+++ solutions/0121-Best-Time-to-Buy-and-Sell-Stock/0121.py
@@ -1,9 +1,8 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # solution one: 贪心
         profit, buy = 0, 0x7FFFFFFF
         for price in prices:
             if buy > price:
@@ -11,7 +10,16 @@ class Solution(object):
             if profit < price - buy:
                 profit = price - buy  # 尽量在最大价格卖出股票
         return profit
-                
+
+        # solution two: 动态规划
+        # K = 1
+        dp_i_0 = 0
+        dp_i_1 = float('-inf')  # 负无穷
+        for i in range(len(prices)):
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])  # 前面没有股票，前面有股票现在卖出
+            dp_i_1 = max(dp_i_1, -prices[i])  # 前面有股票，前面没有股票现在买入
+        return dp_i_0
+  
 if __name__ == "__main__":
     prices = [7,1,5,3,6,4]
     print(Solution().maxProfit(prices))
