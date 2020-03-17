4a9a078bbc90c20aea02330a151014064fa48660
Wonz5130 <wangzhou5130@gmail.com>
2020-03-17 20:56:42 +0800

    :green_heart:Upload 0240 Code
    :green_heart:Upload 0240 Code

---
+++ solutions/0240-Search-a-2D-Matrix-II/0240.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        # solution one: 暴力
+        # 特判
+        if matrix == []:
+            return False
+        
+        n, m = len(matrix), len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == target:
+                    return True
+                elif matrix[i][j] > target:
+                    break
+        return False
+
+        # solution two: 左下角标志数法
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                i -= 1
+            else:
+                j += 1
+        return False
