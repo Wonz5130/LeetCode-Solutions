d16fe392044491f8894731d57f4f71ed1efe7688
Wonz5130 <wangzhou5130@gmail.com>
2020-03-18 21:50:10 +0800

    :green_heart:Update 0836 Code
    :green_heart:Update 0836 Code

--- solutions/0836-Rectangle-Overlap/0836.py
+++ solutions/0836-Rectangle-Overlap/0836.py
@@ -1,6 +1,6 @@
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
-        # 左小角取 max
+        # 左下角取 max
         x1 = max(rec1[0], rec2[0])
         y1 = max(rec1[1], rec2[1])
         # 右上角取 min
