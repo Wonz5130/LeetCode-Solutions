f79be100f2301553a537cfb6595c5207125402f5
Wonz5130 <wangzhou5130@gmail.com>
2020-03-22 09:42:15 +0800

    :green_heart:Update 1385 Code
    :green_heart:Update 1385 Code

--- solutions/1385-Find-the-Distance-Value-Between-Two-Arrays/1385.py
+++ solutions/1385-Find-the-Distance-Value-Between-Two-Arrays/1385.py
@@ -2,6 +2,7 @@ from typing import List
 
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        # solution one: 暴力
         res = 0
         for x in arr1:
             cnt = 0
