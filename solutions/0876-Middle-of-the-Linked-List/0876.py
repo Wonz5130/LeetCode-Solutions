34755fdb71fc672477d24d33866dcda80bf3f1ef
Wonz5130 <wangzhou5130@gmail.com>
2020-03-23 22:31:52 +0800

    :green_heart:Upload 0876 Code
    :green_heart:Upload 0876 Code

---
+++ solutions/0876-Middle-of-the-Linked-List/0876.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        
+        # 快慢指针
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
