864e3767d7f80522caa00b96cfcc6f283683172a
Wonz5130 <wangzhou5130@gmail.com>
2020-03-27 19:56:48 +0800

    :green_heart:Update 0509 Code
    :green_heart:Update 0509 Code

--- solutions/0509-Fibonacci-Number/0509.py
+++ solutions/0509-Fibonacci-Number/0509.py
@@ -1,5 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
+        # solution one: 递归
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return (self.fib(n - 1) + self.fib(n - 2)) % 1000000007
+        
+        # solution two: 动态规划
         dp_0, dp_1 = 0, 1
         for _ in range(n):
             dp_0, dp_1 = dp_1, dp_0 + dp_1
