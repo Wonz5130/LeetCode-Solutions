fca0bd21bff03be5b71e59a3a6814fd2220250d1
Wonz5130 <wangzhou5130@gmail.com>
2020-03-27 18:42:31 +0800

    :green_heart:Upload 10 Code
    :green_heart:Upload 10 Code

---
+++ solutions/Interview-10-qing-wa-tiao-tai-jie-wen-ti-lcof/10-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numWays(self, n: int) -> int:
+        # 初始条件和斐波那契数列有区别
+        dp_0, dp_1 = 1, 1
+        for _ in range(n):
+            dp_0, dp_1 = dp_1, dp_0 + dp_1
+        return dp_0 % 1000000007
