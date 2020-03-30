e49c9dea394fde0dfee98900c30a4f3f404988e9
Wonz5130 <wangzhou5130@gmail.com>
2020-03-30 20:12:44 +0800

    :green_heart:Upload 62 Code
    :green_heart:Upload 62 Code

--- solutions/Interview-62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/62.py
+++ solutions/Interview-62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/62.py
@@ -4,6 +4,7 @@ class Solution:
             return None
             
         last = 0
+        # 最后一轮只剩两个人，倒过来推
         for i in range(2, n + 1):
             last = (last + m) % i
         return last
