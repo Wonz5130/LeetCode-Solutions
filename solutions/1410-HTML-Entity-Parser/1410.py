0a7d2bb2c9847d93fe35eab367ad5ed4ed08f562
Wonz5130 <wangzhou5130@gmail.com>
2020-04-12 22:15:21 +0800

    :green_heart:Upload 1410 Code
    :green_heart:Upload 1410 Code

---
+++ solutions/1410-HTML-Entity-Parser/1410.py
@@ -0,0 +1,9 @@
+class Solution:
+    def entityParser(self, text: str) -> str:
+        text = text.replace("&quot;",'"')
+        text = text.replace("&apos;","'")
+        text = text.replace("&gt;",'>')
+        text = text.replace("&lt;",'<')
+        text = text.replace("&frasl;",'/')
+        text = text.replace("&amp;","&")
+        return text
