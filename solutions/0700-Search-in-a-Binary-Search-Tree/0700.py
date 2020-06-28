eb81c8254cd7096c5a60900d5b2278f759b4372b
Wonz5130 <wangzhou5130@gmail.com>
2020-06-28 19:58:34 +0800

    :green_heart:Upload 0700 Code
    :green_heart:Upload 0700 Code

---
+++ solutions/0700-Search-in-a-Binary-Search-Tree/0700.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        # 节点不存在
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        if root.val > val:
+            return self.searchBST(root.left, val)
