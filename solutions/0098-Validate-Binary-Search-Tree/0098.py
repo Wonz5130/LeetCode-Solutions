6ab118eab78ec527a9df7199b2f5ad58f2075a67
Wonz5130 <wangzhou5130@gmail.com>
2020-06-28 20:18:09 +0800

    :green_heart:Upload 0098 Code
    :green_heart:Upload 0098 Code

---
+++ solutions/0098-Validate-Binary-Search-Tree/0098.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        return self.isValid(root, None, None)
+    
+    def isValid(self, root: TreeNode, min_: TreeNode, max_: TreeNode):
+        if not root:
+            return True
+        if min_ != None and root.val <= min_.val:
+            return False
+        if max_ != None and root.val >= max_.val:
+            return False
+        return self.isValid(root.left, min_, root) and self.isValid(root.right, root, max_)
