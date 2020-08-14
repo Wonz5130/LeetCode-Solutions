0cac760e651a698ff51fe0261c6626b99028ca30
Wonz5130 <wangzhou5130@gmail.com>
2020-08-14 14:04:21 +0800

    :green_heart:Upload 54 Code
    :green_heart:Upload 54 Code

---
+++ solutions/Interview-54-er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/54.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        def dfs(root):
+            if not root:
+                return
+            # 中序遍历倒序
+            dfs(root.right)
+            if self.k == 0:
+                return
+            self.k -= 1
+            if self.k == 0:
+                self.res = root.val
+            dfs(root.left)
+        
+        self.k = k
+        dfs(root)
+        return self.res
