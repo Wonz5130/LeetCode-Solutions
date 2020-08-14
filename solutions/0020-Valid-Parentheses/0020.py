21e6427a9524dac1eb4205150572e05b1c1da511
Wonz5130 <wangzhou5130@gmail.com>
2020-08-14 15:25:53 +0800

    :green_heart:Upload 0020 Code
    :green_heart:Upload 0020 Code

---
+++ solutions/0020-Valid-Parentheses/0020.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'}': '{', ']': '[', ')': '('}
+        stack = []
+        for c in s:
+            if stack and c in dic:
+                if stack[-1] == dic[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
