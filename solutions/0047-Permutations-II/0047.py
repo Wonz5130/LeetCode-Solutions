066f3e920633fff604fb44aef3ebf9a722e2b810
Wonz5130 <wangzhou5130@gmail.com>
2020-09-10 16:05:11 +0800

    :green_heart:Update 0047 Code
    :green_heart:Update 0047 Code

--- solutions/0047-Permutations-II/0047.py
+++ solutions/0047-Permutations-II/0047.py
@@ -39,6 +39,37 @@ class Solution:
             check[i] = 1
             self.dfs(sol + [nums[i]], nums, check)
             check[i] = 0  # after backtracking, should update check[i]
+        
+        # solution three: backtracking
+        res = []
+        n = len(nums)
+        visited = [0] * n
+        # 先排序
+        nums.sort()
+
+        def backtrack(nums, path):
+            # 1. 递归终止情况
+            if len(path) == n:
+                res.append(path[:])
+                return
+            
+            for i in range(n):
+                # 3. 剪枝
+                # 剪掉这种情况：已经访问过
+                if visited[i] == 1:
+                    continue
+                # 剪掉这种情况：前一个数字没有被访问过且当前数字和前一个数字相同
+                if i > 0 and nums[i] == nums[i - 1] and not visited[i - 1]:
+                    continue
+                # 2. 回溯以及更新 path
+                visited[i] = 1
+                path.append(nums[i])
+                backtrack(nums, path)
+                path.pop()
+                visited[i] = 0
+        
+        backtrack(nums, [])
+        return res
 
 if __name__ == "__main__":
     nums = [1,1,2]
