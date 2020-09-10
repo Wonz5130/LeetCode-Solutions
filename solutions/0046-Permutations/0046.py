745e2d2161209667c3b7cce07aed688692526015
Wonz5130 <wangzhou5130@gmail.com>
2020-09-10 11:31:40 +0800

    :green_heart:Update 0046 Code
    :green_heart:Update 0046 Code

--- solutions/0046-Permutations/0046.py
+++ solutions/0046-Permutations/0046.py
@@ -35,6 +35,32 @@ class Solution:
         dfs([])
         return res
 
+        # solution four: backtracking
+        res = []
+        n = len(nums)
+        visited = [0] * n
+        def backtrack(nums, path):
+            # 1. 递归终止情况
+            if len(path) == n:
+                res.append(path[:])
+                return
+            for i in range(n):
+                # 3. 剪枝
+                # 下面这种方法不够高效
+                # if nums[i] in path:
+                #     continue
+                # 使用 visited 来标记是否已经被访问过
+                if not visited[i]:
+                    visited[i] = 1
+                    # 2. 回溯以及更新 path
+                    path.append(nums[i])
+                    backtrack(nums, path)
+                    path.pop()
+                    visited[i] = 0
+        
+        backtrack(nums, [])
+        return res
+
 if __name__ == "__main__":
     nums = [1,2,3]
     print(Solution().permute(nums))        
