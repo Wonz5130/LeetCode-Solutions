65392b7863acbb0d0283c6c78bdb423b46fed794
Wonz5130 <wangzhou5130@gmail.com>
2021-01-07 21:01:14 +0800

    :green_heart:Upload 0538 Code
    :green_heart:Upload 0538 Code

---
+++ solutions/0538-Convert-BST-to-Greater-Tree/0538.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        def dfs(root):
+            nonlocal sumval
+            if root:
+                dfs(root.right)
+                sumval += root.val
+                root.val = sumval  # 将BST转化成累加树
+                dfs(root.left)
+        
+        sumval = 0
+        dfs(root)
+        return root
