9e7bff79e69a68ce63aa63ca9cc7d7d612dc2f15
Wonz5130 <wangzhou5130@gmail.com>
2021-01-21 21:28:50 +0800

    :green_heart:Upload 0783 Code
    :green_heart:Upload 0783 Code

---
+++ solutions/0783-Minimum-Distance-Between-BST-Nodes/0783.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        # solution one: dfs遍历取节点值，再单独计算最小绝对差
+        def dfs(root):
+            if not root:
+                return
+            # 中序遍历是递增的
+            if root.left:
+                dfs(root.left)
+            tmp_val.append(root.val)
+            if root.right:
+                dfs(root.right)
+        tmp_val = []
+        dfs(root)
+        res = float("inf")
+        for i in range(len(tmp_val) - 1):
+            res = min(res, abs(tmp_val[i] - tmp_val[i + 1]))
+        return res
+
+        # solution two: dfs遍历直接进行绝对值比较
+        pre = -1
+        res = float("inf")
+        def dfs(root):
+            nonlocal pre, res
+            if not root:
+                return
+            # 中序遍历是递增的
+            if root.left:
+                dfs(root.left)
+            if pre != -1:
+                res = min(res, abs(pre - root.val))
+            pre = root.val
+            if root.right:
+                dfs(root.right)
+        dfs(root)
+        return res
