1c858f47699553294cf504b47ecc80e45b6ba8aa
Wonz5130 <wangzhou5130@gmail.com>
2021-03-14 13:57:25 +0800

    :green_heart:Upload 0408 Code
    :green_heart:Upload 0408 Code

---
+++ solutions/Interview-04.08-First-Common-Ancestor-LCCI/0408.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or root == p or root == q:
+            return root
+            
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # 左右子树都为空
+        if not left and not right:
+            return None
+        # 右子树为空
+        elif left and not right:
+            return left
+        # 左子树为空
+        elif not left and right:
+            return right
+        # 左右子树都不为空
+        else:
+            return root
