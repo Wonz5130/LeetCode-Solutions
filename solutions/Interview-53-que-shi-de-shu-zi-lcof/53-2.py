7474f016cdb7b45246ae66c01973e946786cbc4c
Wonz5130 <wangzhou5130@gmail.com>
2021-03-15 22:27:51 +0800

    :green_heart:Upload 53-2 Code
    :green_heart:Upload 53-2 Code

---
+++ solutions/Interview-53-que-shi-de-shu-zi-lcof/53-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+        # 返回left
+        return left
